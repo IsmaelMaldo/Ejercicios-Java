70e5a5a328552fa7f98a6decac653085eee3916e
Ismael Maldonado González <isma.forsaken@gmail.com>
2016-10-06 13:56:41 +0200

    Hecho el ejercicio 1

---
+++ Tema 4/Ejercicio01.java
@@ -0,0 +1,23 @@
+/*
+* Ejercicio 1
+* 
+* @author Ismael Maldonado González
+*/
+
+public class Ejercicio01 {
+  public static void main(String[] args){
+    System.out.println("Escribe un día de la semana");
+    String dia = System.console().readLine();
+    if (dia.toLowerCase().equals("lunes")) {
+      System.out.println("A primera toca Sistemas Infromáticos");
+    } else if (dia.toLowerCase().equals("martes") || dia.toLowerCase().equals("miercoles")) {
+      System.out.println("A primera toca Programación");
+    } else if (dia.toLowerCase().equals("jueves")) {
+      System.out.println("A primera toca Base de Datos");
+    } else if (dia.toLowerCase().equals("viernes")) {
+      System.out.println("A primera toca Lenguaje de Marcas y Sistemas de Gestión de información");
+    } else {
+      System.out.println("Ese día no hay clases :D");
+    }
+  }
+}
