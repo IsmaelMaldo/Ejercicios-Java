9c22b70d95bb3e5ca4301417d1fff4f0297f2a34
Ismael Maldonado González <isma.forsaken@gmail.com>
2016-10-08 17:44:02 +0200

    Arreglado ejercicio 2

--- Tema 4/Ejercicio02.java
+++ Tema 4/Ejercicio02.java
@@ -9,13 +9,15 @@
 public class Ejercicio02 {
   public static void main(String[] args){
     System.out.println("¿Qué hora es?");
-    int hora = Integer.parseInt(System.console().readLine())7;
+    int hora = Integer.parseInt(System.console().readLine());
     if (hora >= 6 && hora <= 12) {
       System.out.println("Buenos Días");
     } else if (hora >= 12 && hora <= 20) {
       System.out.println("Buenas Tardes");
+    } else if ((hora >= 21 && hora <= 24) || (hora >= 0 && hora <=5) ) {
+      System.out.println("Buenas noches");
     } else {
-      System.out.println("Buenas Noches");
+      System.out.println("Esa hora no exsiste");
     }
   }
 }
