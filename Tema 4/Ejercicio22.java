9db0f64800905f1631d4a54ed878116cb3e82ded
Ismael Maldonado González <isma.forsaken@gmail.com>
2016-10-11 18:52:11 +0200

    corregido Ejercicio 22 del tema 4

--- Tema 4/Ejercicio22.java
+++ Tema 4/Ejercicio22.java
@@ -7,28 +7,33 @@
 * @author Ismael Maldonado González
 */
 
-import java.util.Scanner;
-
 public class Ejercicio22 {
   public static void main(String[] args){
-    Scanner s = new Scanner(System.in);
-    System.out.println("Escribe la nota del primer examen");
-    double examen1 = s.nextDouble();
-    System.out.println("Escribe la nota del segundo examen");
-    double examen2 = s.nextDouble();
-    double nota_final = (examen1 + examen2) / 2;
+    System.out.println("¿En qué día de la semana estámos?");
+    String dia = System.console().readLine();
+    System.out.println("¿Qué hora es?");
+    int hora = Integer.parseInt(System.console().readLine());
+    System.out.println("¿Y cuántos minutos?");
+    int minutos = Integer.parseInt(System.console().readLine());
+    int dia_num = 0;
     
-    if (nota_final >= 5) {
-      System.out.println("Tu nota de Programación es " + nota_final);
-    } else {
-      System.out.println("¿Cuál ha sido el resultado de la recuperación?");
-      String recuperacion = System.console().readLine();
-      
-      if (recuperacion.toLowerCase().equals("apto")) {
-        System.out.println("Tu nota de Programación es un 5");
-      } else {
-        System.out.println("Tu nota de Programación es un " + nota_final);
-      }
+    switch (dia.toLowerCase()){
+      case "lunes":
+        dia_num = 0;
+        break;
+      case "martes":
+        dia_num = 1;
+        break;
+      case "miercoles":
+        dia_num = 2;
+        break;
+      case "jueves":
+        dia_num = 3;
+        break;
+      case "viernes":
+        dia_num = 4;
+        break;
     }
+    System.out.printf("Quedan %d minutos para el fin de semana. ¡Ánimo!\n", + (6660 - ((dia_num * 24 * 60) + (hora * 60) + minutos)));
   }
 }
