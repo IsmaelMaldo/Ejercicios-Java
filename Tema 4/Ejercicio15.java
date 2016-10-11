e404ad0f79f910aa13dbe4f9d89a240baf8f6cff
Ismael Maldonado González <isma.forsaken@gmail.com>
2016-10-11 16:03:18 +0200

    Añadido Ejercicio 15 del tema 4

---
+++ Tema 4/Ejercicio15.java
@@ -0,0 +1,40 @@
+/*
+* Ejercicio 15
+*
+* Pinta una pirámide con un caracter dado
+* 
+* 
+* @author Ismael Maldonado González
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Escribe un caracter");
+    String caracter = s.nextLine();
+    System.out.println("¿Como quieres la pirámide?");
+    System.out.println("1. Apuntando hacia arriba");
+    System.out.println("2. Apuntando hacia abajo");
+    int opcion = s.nextInt();
+    switch (opcion) {
+      case 1:
+        System.out.println("     " + caracter);
+        System.out.println("    " + caracter + caracter + caracter);
+        System.out.println("   " + caracter + caracter + caracter + caracter + caracter);
+        System.out.println("  " + caracter + caracter + caracter + caracter + caracter + caracter + caracter);
+        System.out.println(" " + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter);
+        System.out.println(caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter);
+        break;
+      case 2:
+        System.out.println(caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter);
+        System.out.println(" " + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter + caracter);
+        System.out.println("  " + caracter + caracter + caracter + caracter + caracter + caracter + caracter);
+        System.out.println("   " + caracter + caracter + caracter + caracter + caracter);
+        System.out.println("    " + caracter + caracter + caracter);
+        System.out.println("     " + caracter);
+        break;
+    }
+  }
+}
