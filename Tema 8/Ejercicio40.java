53659bad6f314313f28a92020394f2256fe5e4bf
Ismael Maldonado González <isma.forsaken@gmail.com>
2017-02-01 12:19:17 +0100

    Añadidos ejercicios

--- Tema 8/Ejercicio40.java
+++ Tema 8/Ejercicio40.java
@@ -28,35 +28,37 @@ public class Ejercicio40 {
   */
   public static int[] filtraCon7(int[] n) {
     int contador = 0;
-    for (int i = 0; i < n.length; i++){
-      boolean hay7 = false;
-      int aux = n[i];
-      while (aux > 0 && !hay7) {
-        int dig = aux % 10;
-        aux = aux / 10;
-        if (dig == 7){
+    for (int x : n){
+        if (hay7(x)){
           contador++;
-          hay7 = true;
         }
-      }
     }
     int[] num7 = new int[contador];
     contador = 0;
-    for (int i = 0; i < n.length; i++){
-      boolean hay7 = false;
-      int aux = n[i];
-      while (aux > 0 && !hay7) {
-        int dig = aux % 10;
-        aux = aux / 10;
-        if (dig == 7){
-          hay7 = true;
-        }
-      }
-      if (hay7) {
-        num7[contador] = n[i];
+    for (int x : n){
+      if (hay7(x)) {
+        num7[contador] = x;
         contador++;
       }
     }
     return num7;
+  }
+
+  /*
+   * Devuelve verdadero o falso dependiendo de si el número dado tiene
+   * el dígito 7 o no
+   *
+   * @param num Número entero
+   * @return <code>true</code> Si está el dígito 7
+   * @return <code>false</code> Si no está el dígito 7
+  */
+  public static boolean hay7(int num) {
+    while (num > 0) {
+      if ((num % 10) == 7){
+        return true;
+      }
+      num = num / 10;
+    }
+    return false;
   }
 }
